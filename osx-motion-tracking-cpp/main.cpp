d12efac2bbbabe4b96f3e907a2fec12385be3216
Wyatt Pfeil <wyatt.pfeil@gmail.com>
2018-04-07 15:58:19 -0400

    Initial Commit

---
+++ osx-motion-tracking-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  osx-motion-tracking-cpp
+//
+//  Created by Brian Pfeil on 4/7/18.
+//  Copyright © 2018 Brian Pfeil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
